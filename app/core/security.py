b02c77753a44122e78362e1042b1fe9f21593b7f
melika <melikatavakoliedu@gmail.com>
2026-06-27 15:29:18 +0330

    feat: configs and token

---
+++ app/core/security.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+from passlib.context import CryptContext
+
+from app.core.config import settings
+
+pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
+ALGORITHM=settings.JWT_ALGORITHM
+
+def hash_password(password:str)->str:
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed: str)->bool:
+    return pwd_context.verify(password, hashed)
+
+def create_acccess_token(subject: str):
+    expire=datetime.now(timezone.utc)+timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload={
+        "sub": subject,
+        "exp": expire,
+        "type": "access",
+    }
+    return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(subject: str):
+    expire=datetime.now(timezone.utc)+timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+    payload={
+        "sub": subject,
+        "exp": expire,
+        "type": "refresh",
+    }
+    return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(token: str):
+    return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[ALGORITHM])
